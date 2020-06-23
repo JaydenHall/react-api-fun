030a62d633947e0ccda65b3ac5ba7c4dfdc6238d
Nick Jarvie <62858674+nick-jarvie@users.noreply.github.com>
2020-06-24 11:04:45 +1200

    create joke api

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import request from 'superagent'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+
+  state = {
+    setUp: '',
+    delivery: '',
+    joke: ''
+  }
+
+  componentDidMount() {
+    // this.getMyQuote()
+  }
+
+  handleClick = () => {
+    this.getMyQuote()
+  }
+
+  getMyQuote = () => {
+    request.get('https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=racist,sexist')
+      .then(res => {
+        console.log(res.body);
+        this.setState({
+          setUp: res.body.setup,
+          delivery: res.body.delivery,
+          joke: res.body.joke
+        })
+        console.log(this.state);
+
+      })
+  }
+  render() {
+    return (
+      <>
+        <h1>Joke api test</h1>
+        <button onClick={this.handleClick}>Get Joke</button>
+        <p>{this.state.setUp}</p>
+        <p>{this.state.delivery}</p> 
+        <p>{this.state.joke}</p>
+      </>
+    )
+  }
 }
-
 export default App
